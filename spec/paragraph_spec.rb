efad25842a1119ad499aa5c03602bd4fa4928eaa
Chris Mear <chris@feedmechocolate.com>
2014-06-06 15:33:33 +0100

    Convert specs to RSpec 2.99.0 syntax with Transpec
    This conversion is done by Transpec 2.2.1 with the following command:
        transpec
    
    * 44 conversions
        from: obj.should
          to: expect(obj).to
    
    * 19 conversions
        from: == expected
          to: eq(expected)
    
    For more details: https://github.com/yujinakayama/transpec#supported-conversions

--- spec/paragraph_spec.rb
+++ spec/paragraph_spec.rb
@@ -13,7 +13,7 @@ describe Rticles::Paragraph do
       paragraphs.each{|p| @document.top_level_paragraphs.push(p)}
       @document.save
       @document.reload
-      @document.top_level_paragraphs.map(&:position).should == [1, 2, 3, 4]
+      expect(@document.top_level_paragraphs.map(&:position)).to eq([1, 2, 3, 4])
     end
   end
 
@@ -27,14 +27,14 @@ describe Rticles::Paragraph do
       child = Rticles::Paragraph.new
       @paragraph.children.push(child)
       child.reload
-      child.parent_id.should == @paragraph.id
+      expect(child.parent_id).to eq(@paragraph.id)
     end
 
     it "are associated with their parent's document" do
       child = Rticles::Paragraph.new
       @paragraph.children.push(child)
       child.reload
-      child.document_id.should == @document.id
+      expect(child.document_id).to eq(@document.id)
     end
   end
 
@@ -48,36 +48,36 @@ describe Rticles::Paragraph do
       paragraph = @document.paragraphs.build(:body => "New", :before_id => @document.top_level_paragraphs.first.id)
       paragraph.save!
       @document.reload
-      @document.top_level_paragraphs.map{|p| [p.parent_id, p.position, p.body]}.should == [
+      expect(@document.top_level_paragraphs.map{|p| [p.parent_id, p.position, p.body]}).to eq([
         [nil, 1, "New"],
         [nil, 2, "Originally 1"],
         [nil, 3, "Originally 2"],
         [nil, 4, "Originally 3"]
-      ]
+      ])
     end
 
     it "inserts a paragraph at the bottom" do
       paragraph = @document.paragraphs.build(:body => "New", :after_id => @document.top_level_paragraphs.last.id)
       paragraph.save!
       @document.reload
-      @document.top_level_paragraphs.map{|p| [p.parent_id, p.position, p.body]}.should == [
+      expect(@document.top_level_paragraphs.map{|p| [p.parent_id, p.position, p.body]}).to eq([
         [nil, 1, "Originally 1"],
         [nil, 2, "Originally 2"],
         [nil, 3, "Originally 3"],
         [nil, 4, "New"]
-      ]
+      ])
     end
 
     it "inserts a paragraph in the middle" do
       paragraph = @document.paragraphs.build(:body => "New", :before_id => @document.top_level_paragraphs[1].id)
       paragraph.save!
       @document.reload
-      @document.top_level_paragraphs.map{|p| [p.parent_id, p.position, p.body]}.should == [
+      expect(@document.top_level_paragraphs.map{|p| [p.parent_id, p.position, p.body]}).to eq([
         [nil, 1, "Originally 1"],
         [nil, 2, "New"],
         [nil, 3, "Originally 2"],
         [nil, 4, "Originally 3"]
-      ]
+      ])
     end
 
     context "into a list of children" do
@@ -90,36 +90,36 @@ describe Rticles::Paragraph do
         paragraph = @document.paragraphs.build(:body => "New", :before_id => @first_top_paragraph.children.first.id)
         paragraph.save!
         @first_top_paragraph.reload
-        @first_top_paragraph.children.map{|p| [p.parent_id, p.position, p.body]}.should == [
+        expect(@first_top_paragraph.children.map{|p| [p.parent_id, p.position, p.body]}).to eq([
           [@first_top_paragraph.id, 1, "New"],
           [@first_top_paragraph.id, 2, "Child originally 1"],
           [@first_top_paragraph.id, 3, "Child originally 2"],
           [@first_top_paragraph.id, 4, "Child originally 3"]
-        ]
+        ])
       end
 
       it "inserts a paragraph at the bottom" do
         paragraph = @document.paragraphs.build(:body => "New", :after_id => @first_top_paragraph.children.last.id)
         paragraph.save!
         @first_top_paragraph.reload
-        @first_top_paragraph.children.map{|p| [p.parent_id, p.position, p.body]}.should == [
+        expect(@first_top_paragraph.children.map{|p| [p.parent_id, p.position, p.body]}).to eq([
           [@first_top_paragraph.id, 1, "Child originally 1"],
           [@first_top_paragraph.id, 2, "Child originally 2"],
           [@first_top_paragraph.id, 3, "Child originally 3"],
           [@first_top_paragraph.id, 4, "New"]
-        ]
+        ])
       end
 
       it "inserts a paragraph in the middle" do
         paragraph = @document.paragraphs.build(:body => "New", :before_id => @first_top_paragraph.children[1].id)
         paragraph.save!
         @first_top_paragraph.reload
-        @first_top_paragraph.children.map{|p| [p.parent_id, p.position, p.body]}.should == [
+        expect(@first_top_paragraph.children.map{|p| [p.parent_id, p.position, p.body]}).to eq([
           [@first_top_paragraph.id, 1, "Child originally 1"],
           [@first_top_paragraph.id, 2, "New"],
           [@first_top_paragraph.id, 3, "Child originally 2"],
           [@first_top_paragraph.id, 4, "Child originally 3"]
-        ]
+        ])
       end
     end
   end
@@ -129,16 +129,16 @@ describe Rticles::Paragraph do
       @document = Rticles::Document.create
       @tlp = @document.top_level_paragraphs.create(:body => "top-level")
       3.times{@document.paragraphs.create(:parent_id => @tlp.id)}
-      @document.paragraphs.map{|p| [p.parent_id, p.position]}.should eq [
+      expect(@document.paragraphs.map{|p| [p.parent_id, p.position]}).to eq [
         [nil, 1],
         [@tlp.id, 1],
         [@tlp.id, 2],
         [@tlp.id, 3]
       ]
 
-      @document.paragraphs.count.should eq 4
+      expect(@document.paragraphs.count).to eq 4
       @document.reload.top_level_paragraphs.first.destroy
-      @document.reload.paragraphs.count.should == 0
+      expect(@document.reload.paragraphs.count).to eq(0)
     end
   end
 
@@ -146,13 +146,13 @@ describe Rticles::Paragraph do
     it "makes the paragraph a child of its previous sibling" do
       stub_outline([:one, :two])
       @document.top_level_paragraphs[1].indent!
-      @document.reload.outline.should == ['one', ['two']]
+      expect(@document.reload.outline).to eq(['one', ['two']])
     end
 
     it "goes at the bottom of the previous sibling's children" do
       stub_outline([:one, [:sub_one, :sub_two], :two])
       @document.top_level_paragraphs[1].indent!
-      @document.reload.outline.should == ['one', ['sub_one', 'sub_two', 'two']]
+      expect(@document.reload.outline).to eq(['one', ['sub_one', 'sub_two', 'two']])
     end
   end
 
@@ -160,13 +160,13 @@ describe Rticles::Paragraph do
     it "inserts itself back into its parent's level" do
       stub_outline([:one, [:two]])
       @document.top_level_paragraphs[0].children[0].outdent!
-      @document.reload.outline.should == ['one', 'two']
+      expect(@document.reload.outline).to eq(['one', 'two'])
     end
 
     it "splits its siblings" do
       stub_outline [:one, [:sub_one, :sub_two, :sub_three], :two]
       @document.top_level_paragraphs[0].children[1].outdent!
-      @document.reload.outline.should == ['one', ['sub_one'], 'sub_two', ['sub_three'], 'two']
+      expect(@document.reload.outline).to eq(['one', ['sub_one'], 'sub_two', ['sub_three'], 'two'])
     end
   end
 
@@ -183,16 +183,16 @@ describe Rticles::Paragraph do
     end
 
     it "ignores headings" do
-      @document.paragraphs[0].index.should eq 1
-      @document.paragraphs[2].index.should eq 2
+      expect(@document.paragraphs[0].index).to eq 1
+      expect(@document.paragraphs[2].index).to eq 2
     end
 
     it "returns nil for headings" do
-      @document.paragraphs[1].index.should be_nil
+      expect(@document.paragraphs[1].index).to be_nil
     end
 
     it "ignores paragraphs omitted by choices" do
-      @document.paragraphs[4].index(@document.choices).should eq 3
+      expect(@document.paragraphs[4].index(@document.choices)).to eq 3
     end
   end
 
@@ -245,7 +245,7 @@ describe Rticles::Paragraph do
 
       html = @document.to_html
 
-      html.should be_equivalent_to(expected_html)
+      expect(html).to be_equivalent_to(expected_html)
     end
 
     context "with insertions" do
@@ -286,7 +286,7 @@ describe Rticles::Paragraph do
 
         html = @document.to_html
 
-        html.should be_equivalent_to(expected_html)
+        expect(html).to be_equivalent_to(expected_html)
       end
     end
 
@@ -330,7 +330,7 @@ describe Rticles::Paragraph do
 
         html = @document.to_html
 
-        html.should be_equivalent_to(expected_html)
+        expect(html).to be_equivalent_to(expected_html)
       end
 
       it "excludes the 'true' clause when the choice is set to false" do
@@ -364,7 +364,7 @@ describe Rticles::Paragraph do
 
         html = @document.to_html
 
-        html.should be_equivalent_to(expected_html)
+        expect(html).to be_equivalent_to(expected_html)
       end
 
       context "when the choices cause an entire paragraph group to be omitted" do
@@ -420,7 +420,7 @@ describe Rticles::Paragraph do
 
         html = @document.to_html(:with_index => false)
 
-        html.should be_equivalent_to(expected_html)
+        expect(html).to be_equivalent_to(expected_html)
       end
     end
   end
